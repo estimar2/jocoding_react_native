df693a89953b159d82e645c0d79ed182879bd8ce
estimar2 <songsm5504@gmail.com>
2024-06-19 17:45:42 +0900

    [24.06.19] #2.5 Styles

--- App.js
+++ App.js
@@ -4,12 +4,48 @@ export default function App() {
   return (
     // Container View 가 Flex Container
     // Flex Direction 기본값은 모두 Column
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 2, backgroundColor: "red" }}></View>
-      <View style={{ flex: 3, backgroundColor: "orange" }}></View>
-      <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>대전</Text>
+      </View>
+
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.discription}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f3e228",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 38,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  discription: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+});
